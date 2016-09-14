93c62f7674d1f8ac6e27c642d49785cab7f1ea34
akzaidi <alikaz.zaidi@gmail.com>
2016-09-14 12:20:17 -0400

    remove csv files

---
+++ 0-pkgs-prereqs.R
@@ -0,0 +1,29 @@
+# install packages ---
+
+# update repo
+r <- getOption('repos')
+# set mirror to something a bit more recent
+r["CRAN"] <- "https://mran.revolutionanalytics.com/snapshot/2016-09-12/"
+options(repos = r)
+
+# download shapefiles from: http://www.zillow.com/howto/api/neighborhood-boundaries.htm
+download.file("http://www.zillow.com/static/shp/ZillowNeighborhoods-NY.zip", destfile = "data/")
+
+# install R packages
+pkgs_to_install <- c("devtools",
+                      "data.table",
+                     "stringr",
+                     "broom", "magrittr", "dplyr",
+                     "lubridate",
+                      "rgeos", "sp", "maptools",
+                      "seriation",
+                     "ggplot2", "gridExtra",
+                     "ggrepel",
+                     "tidyr", "revealjs"
+                     )
+
+pks_missing <- pkgs_to_install[!(pkgs_to_install %in% installed.packages()[, 1])]
+
+install.packages(pks_missing)
+
+# ---
